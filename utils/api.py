3fa1fce7e294e164c8024958289e15fbbbfa1cde
Timothy Morrissey <timothy.morrissey@outlook.com>
2025-10-23 15:13:17 -0400

    Update api.py

--- utils/api.py
+++ utils/api.py
@@ -4,7 +4,6 @@ import requests
 # 	--url 'https://free-api-live-football-data.p.rapidapi.com/football-players-search?search=m' 
 # 	--header 'x-rapidapi-host: free-api-live-football-data.p.rapidapi.com' 
 # 	--header 'x-rapidapi-key: 3ce4317182msh1ec30ae960bb2a9p1bd7f7jsnf6c1a283ec9b'
-FOOTIE_DATA_API_KEY='3ce4317182msh1ec30ae960bb2a9p1bd7f7jsnf6c1a283ec9b'
 def get_live_matches():
     url = "https://api-football-v1.p.rapidapi.com/v3/fixtures?live=all"
     headers = {
